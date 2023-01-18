bf12022bc842d134ab640af46e54cf69dbcbb777
Cesar Guillermo Vazquez Alvarez <cdgva@outlook.com>
2023-01-17 19:00:56 -0600

    Se comprueban que sea un grupo por asociatividad y elementos inversos

--- Inverso_grupo.py
+++ Inverso_grupo.py
@@ -20,73 +20,76 @@ def phi(n):
 
 
 def inverso_euler(U, n):
-    listInv= []
+    listInv = []
     for a in U:
         fi = phi(n)
         listInv.append(pow(a, fi - 1) % n)
     return listInv
 
-def inversos_fermat(G, p):
+
+def inverso_fermat(G, p):
+    listInv = []
     for a in G:
-        print(pow(a, p - 2) % p)
+        listInv.append(pow(a, p - 2) % p)
+    return listInv
 
 
 def asociatividad(S, n, u=False):
-    for a in (S):
+    for a in S:
         for b in S:
             for c in S:
                 if ((a * b) * c) % n != (a * (b * c)) % n:
                     return "No tiene asociatividad"
     return f"El grupo {'U' if u else 'Z'}, n = {n}.\nSi tiene asociatividad.\n"
 
-def inversos(U,n):
+
+def inversos(U, n):
     if len(inverso_euler(U, n)) == len(U):
         print("Todos los elementos del grupo tienen inversos.")
     else:
         print("No todos los elementos del grupo tienen inversos.")
 
+
 def main():
-    '''
-
-    n = 131
-    U = list(range(50, 61))
-    inversos_fermat(U, n)
-    inverso_euler(U, n)
-
-    U = calcularU(12)
-    inversos_fermat(U, 12)
-    inverso_euler(U, 12)
-
-    inversos_fermat([127], 45775)
-    inverso_euler([127], 45775)
-    '''
-    #1) U_125
+    """ 1.- Verificar que los siguientes conjuntos con la operación de multiplicación (módulo n) son grupos o no."""
+    # 1) U_125
     n = 125
-    print('-',n)
+    print('-', n)
     U = calcularU(n)
-    inversos(U,n)
+    inversos(inverso_euler(U, n), n)
     print(asociatividad(U, n, True))
 
-    #2) U_1023
+    # 2) U_1023
     n = 1023
-    print('-',n)
+    print('-', n)
     U = calcularU(n)
-    inversos(U,n)
+    inversos(inverso_euler(U, n), n)
     print(asociatividad(U, n, True))
 
-    #3) Z_203
+    # 3) Z_203
     n = 203
-    print('-',n)
-    Z = list(range(1,n))
-    inversos(U,n)
-    print(asociatividad(U, n))
+    print('-', n)
+    Z = list(range(1, n))
+    inversos(inverso_euler(Z, n), n)
+    print(asociatividad(Z, n))
 
-    #4) Z_1021
+    # 4) Z_1021
     n = 1021
-    print('-',n)
+    print('-', n)
     Z = list(range(1, n + 1))
-    inversos(U,n)
-    print(asociatividad(U, n))
+    inversos(inverso_euler(Z, n), n)
+    print(asociatividad(Z, n))
+
+    """ 2.- Calcular los inversos del grupo U_131 de los elementos:"""
+    # a = 50, 51, 52, ..., 60
+    n = 131
+    print('-', n,"\na = [50,51...,60]")
+    U = list(range(50, 61))
+    print("Fermat:")
+    print(inverso_fermat(U, n))
+    print("Euler:")
+    print(inverso_euler(U, n))
+
 
 if __name__ == '__main__':
     main()
