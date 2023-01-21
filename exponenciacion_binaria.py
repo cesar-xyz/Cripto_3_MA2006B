8cdd26de28fdb8cfe1afaad474e505ca3e40c492
Cesar Guillermo Vazquez Alvarez <cdgva@outlook.com>
2023-01-21 13:43:15 -0600

    Corrección de bases

--- exponenciacion_binaria.py
+++ exponenciacion_binaria.py
@@ -1,14 +1,15 @@
 def exp(a, n):
+    init_a = a
     for bi in bin(n)[3::]:
         if bi == '0':
             a = a ** 2
         else:
-            a = (a ** 2) * 2
+            a = (a ** 2) * init_a
     return a
 
 
 def main():
-    print(exp(24, 600000) % 94358430989)
+    print(exp(3, 199))
 
 
 if __name__ == '__main__':
